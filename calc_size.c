52289034900e31c235baec1db15ffec51448e7e5
yuanqian999 <33048385+yuanqian999@users.noreply.github.com>
2018-11-06 22:17:00 +0800

    Update calc_size.c

---
+++ calc_size.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+	char buf[20] = "hello world!";
+	int size = sizeof(buf);
+	
+	int len = strlen(buf);
+	
+	printf("%d\n%d",size,len);
+	
+	return 0;
+	
+	
+ } 
